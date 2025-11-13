5d0cdb6207480460035725f427d5d25ea16d984c
maksmakuta <maksmakuta@proton.me>
2025-11-13 11:47:18 +0200

    [0.0.2.5] implement Vertex struct

--- src/graphics/Vertex.cpp
+++ src/graphics/Vertex.cpp
@@ -1,5 +1,24 @@
-//
-// Created by maks on 11/12/25.
-//
-
 #include "Vertex.h"
+
+#include "glad/gl.h"
+
+namespace circuits {
+
+    Vertex::Vertex(const glm::vec2& p, const glm::vec2& t, const glm::vec4& c) : pos(p), uv(t), color(c) {}
+    Vertex::Vertex(const glm::vec2& p) : pos(p), uv(0.0f), color(1.0f) {}
+    Vertex::Vertex(const glm::vec2& p,const glm::vec4& c) : pos(p), uv(0.0f), color(c) {}
+
+    void Vertex::setupAttributes() {
+        constexpr GLsizei stride = sizeof(Vertex);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void *>(offsetof(Vertex, pos)));
+
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void *>(offsetof(Vertex, uv)));
+
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void *>(offsetof(Vertex, color)));
+    }
+
+}
--- src/graphics/Vertex.h
+++ src/graphics/Vertex.h
@@ -1,13 +1,23 @@
-//
-// Created by maks on 11/12/25.
-//
-
 #ifndef CIRCUITS_VERTEX_H
 #define CIRCUITS_VERTEX_H
 
+#include <glm/glm.hpp>
 
-class Vertex {
-};
+namespace circuits {
 
+    struct Vertex {
+        Vertex() = default;
+        explicit Vertex(const glm::vec2& p);
+        Vertex(const glm::vec2& p,const glm::vec4& c);
+        Vertex(const glm::vec2& p, const glm::vec2& t, const glm::vec4& c);
+
+        static void setupAttributes();
+
+        glm::vec2 pos   = {0.0f, 0.0f};
+        glm::vec2 uv    = {0.0f, 0.0f};
+        glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
+    };
+
+}
 
 #endif //CIRCUITS_VERTEX_H
